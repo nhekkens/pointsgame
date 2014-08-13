fb72b8c42a2f5f2af3b5d549502e388bccec9bb3
Nicolay Hekkens <nicolay@wednesdayagency.com>
2014-08-13 16:06:18 +0100

    Total points are now displayed. ( needs work around for undefined vars )

--- public/app.js
+++ public/app.js
@@ -22,6 +22,16 @@ user.config( function ($routeProvider, $locationProvider) {
 
 });
 
+// Directives
+user.directive('repeatCompleteDirective', function() {
+  return function(scope, element, attrs) {
+    if (scope.$last) {
+      scope.$emit('ngRepeatFinished');
+      console.log('ngrepeat is done, Executing.');
+    }
+  };
+});
+
 // Controllers
 var controllers = {};
 
@@ -58,20 +68,20 @@ controllers.playerController = function playerController( $scope, $route, $http 
   var userId = $route.current.params.playerId;
   console.log(userId);
 
-  // when landing on the page, get all Users and show them
+  // when landing on the page, get User and add to scope.
   $http.get('/api/users/' + userId )
     .success(function(data) {
       $scope.games = data[0].games;
       $scope.name = data[0].user_name;
       console.log(data[0].games);
 
-      // ISOTOPE
+      // ISOTOPE start
       setTimeout(function(){
 
+        console.log('Isotope Init.' );
         var $isoContainer = $('.isotope');
-        // init
         $isoContainer.isotope({
-          // options
+
           itemSelector: '.game',
           layoutMode: 'masonry',
           sortAscending: false,
@@ -97,15 +107,32 @@ controllers.playerController = function playerController( $scope, $route, $http 
           var sortByValue = $(this).attr('data-sort-by');
           $isoContainer.isotope({ sortBy: sortByValue });
         });
-
       }, 500);
+      // ISOTOPE stop
 
     })
     .error(function(data) {
       console.log('Error: ' + data);
     });
 
+  // After ngrepeat, this is will call all funcs for this page
+  $scope.$on('ngRepeatFinished', function() {
 
+    // add up all points and output
+    calcPlayerPoints();
+  });
+
+  function calcPlayerPoints() {
+    var totalPoints = 0;
+
+    angular.forEach($scope.games,function(value,index){
+      if(typeof value.points != 'undefined')
+      console.log(value.points);
+      totalPoints =+ value.points;
+    })
+
+    $('.totalPoints span').html(totalPoints);
+  }
 };
 
 user.controller(controllers);
